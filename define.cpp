e36cb4061efd3d26a8cda95944b70e734d104fd8
Trần Quang Đãng <91641923+quangdang46@users.noreply.github.com>
2022-05-11 00:25:48 +0700

    Add files via upload

---
+++ define.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vll vector<long long>
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define FOR(i,a,b) for(int i=a;i<b;++i)
+#define FORd(i,a,b) for(int i=a-1;i>=b;--i)
+#define reset(x,y) memset(x,y,sizeof(x))
+#define sz(x) ((int)(x).size())
+#define umap unordered_map
+#define vi vector
+#define ii pair<int, int>
+#define fi first
+#define se second
+#define rz(x) resize(x)
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define set_vect(a,n); vector<int>a(n); for(int i=0;i<n;i++)cin>>a[i];
+#define trav(a,x) for(auto &x : a)
+#define EACH(x, a) for (auto& x: a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
