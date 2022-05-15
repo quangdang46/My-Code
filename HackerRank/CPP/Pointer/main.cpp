ed232797adc619f27fbba7cfea553b06f8c30b37
Trần Quang Đãng <91641923+quangdang46@users.noreply.github.com>
2022-05-16 00:19:57 +0700

    Add files via upload

---
+++ HackerRank/CPP/Pointer/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<math.h>
+void update(int *a,int *b) {
+    // Complete this function  
+    int tmp=*a;
+    *a=*b+*a;
+    *b=abs(tmp-*b);
+
+
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
