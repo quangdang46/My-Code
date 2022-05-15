77a665e418177aff69c497fff78b39dbd69ec205
Trần Quang Đãng <91641923+quangdang46@users.noreply.github.com>
2022-05-15 11:34:38 +0700

    Add files via upload

---
+++ HackerRank/Java/Java Exception Handling (Try-catch)/Solution.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args)
+    {
+        try(Scanner in = new Scanner(System.in))
+        {
+            System.out.println(in.nextInt()/in.nextInt());
+        } catch(InputMismatchException ex)
+        {
+            System.out.println(ex.getClass().getName());
+        }
+        catch(ArithmeticException ae)
+        {
+            System.out.println(ae);
+        }
+    }
+
+}
+
+        
+             
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                 
+                  
+                    
+                     
+                  
+                  
+                  
+                        
+                      
+                          
+                        
+                    
+                    
+
+                
+                  
+                    
+                
+               
+                
+            
+        
+
+
