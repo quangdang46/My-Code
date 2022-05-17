9a403facd1322d845ed9a66cb6813af2f464919c
Trần Quang Đãng <91641923+quangdang46@users.noreply.github.com>
2022-05-17 21:47:53 +0700

    Add files via upload

---
+++ HackerRank/CPP/StringStream/main.cpp
@@ -0,0 +1,29 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	// Complete this function
+    vector<int> result;
+    stringstream ss(str);
+    int k;
+    while(ss >> k) {
+        result.push_back(k);
+        if(ss.peek() == ',') {
+            ss.ignore();
+        }
+    }
+    return result;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
