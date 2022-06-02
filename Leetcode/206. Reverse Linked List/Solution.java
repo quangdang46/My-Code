0317b2516d0cd2b9d9e16d6f220d59189d470a96
Trần Quang Đãng <91641923+quangdang46@users.noreply.github.com>
2022-06-02 09:24:29 +0700

    Update Solution.java

--- Leetcode/206. Reverse Linked List/Solution.java
+++ Leetcode/206. Reverse Linked List/Solution.java
@@ -20,3 +20,15 @@ class Solution {
         return head;
     }
 }
+
+class Solution{
+    public ListNode reverseList(ListNode head) {
+        if(head==null) return null;
+        if(head.next==null) return head;
+        ListNode nextNode=head.next;
+        ListNode newHead=reverseList(nextNode);
+        nextNode.next=head;
+        head.next=null;
+        return newHead;
+    }
+}
