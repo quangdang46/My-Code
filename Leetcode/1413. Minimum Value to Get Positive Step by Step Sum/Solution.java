a779268e47dd917faed5b438755ac844a4c13ce3
Trần Quang Đãng <91641923+quangdang46@users.noreply.github.com>
2022-06-06 15:49:29 +0700

    Update Solution.java

--- Leetcode/1413. Minimum Value to Get Positive Step by Step Sum/Solution.java
+++ Leetcode/1413. Minimum Value to Get Positive Step by Step Sum/Solution.java
@@ -1,5 +1,10 @@
 class Solution {
     public int minStartValue(int[] nums) {
-        
+        int min_prefix = 0, prefix = 0;
+        for (int num : nums) {
+            prefix += num;
+            min_prefix = Math.min(min_prefix, prefix);
+        }
+        return 1 - min_prefix;
     }
 }
